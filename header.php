623ec7ae84316b83b949863bc6cca0ae6172422b
Stephen <stephen@doublethink.co.nz>
2015-07-13 20:48:34 +1200

    fixed responsiveness

--- header.php
+++ header.php
@@ -1,50 +1,76 @@
-<html>
-	<head>
-		<title><?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?></title>
-		<link 	rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-		<link rel='stylesheet' href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/material.deep_orange-light_green.min.css"></link>
-		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-		<style>
-		#view-source {
-		  position: fixed;
-		  display: block;
-		  right: 0;
-		  bottom: 0;
-		  margin-right: 40px;
-		  margin-bottom: 40px;
-		  z-index: 900;
-		}
-		</style>
-	</head>
-	<body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
-		<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-			<header class="mdl-layout__header mdl-layout__header--scroll mdl-color--primary">
-				<div class="mdl-layout--large-screen-only mdl-layout__header-row">
-				</div>
-				<div class="mdl-layout__header-row">
-				  <h3><?php bloginfo( 'name' ); ?></h3>&nbsp&nbsp
-				  <h4><?php bloginfo( 'description' ); ?></h4>
-				</div>
-				<div class="mdl-layout--large-screen-only mdl-layout__header-row">
-				</div>
-				<div class="mdl-layout--large-screen-only mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark">
-				  <a href="#overview" class="mdl-layout__tab is-active">Overview</a>
-				  <a href="#features" class="mdl-layout__tab">Features</a>
-				  <a href="#features" class="mdl-layout__tab">Details</a>
-				  <a href="#features" class="mdl-layout__tab">Technology</a>
-				  <a href="#features" class="mdl-layout__tab">FAQ</a>
-				  <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored mdl-shadow--4dp mdl-color--accent" id="add">
-				    <i class="material-icons">add</i>
-				  </button>
-				</div>
-			</header>
-			<div class="mdl-layout__drawer">
-			<span class="mdl-layout-title"><?php bloginfo( 'name' ); ?></span>
-				<nav class="mdl-navigation">
-				  <a href="#overview" class="mdl-navigation__link">Overview</a>
-				  <a href="#features" class="mdl-navigation__link">Features</a>
-				  <a href="#features" class="mdl-navigation__link">Details</a>
-				  <a href="#features" class="mdl-navigation__link">Technology</a>
-				  <a href="#features" class="mdl-navigation__link">FAQ</a>
-				</nav>
-			</div>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="A front-end template that helps you build fast, modern mobile web apps.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?></title>
+
+    <!-- Add to homescreen for Chrome on Android -->
+    <meta name="mobile-web-app-capable" content="yes">
+    <link rel="icon" sizes="192x192" href="images/touch/chrome-touch-icon-192x192.png">
+
+    <!-- Add to homescreen for Safari on iOS -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="Material Design Lite">
+    <link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png">
+
+    <!-- Tile icon for Win8 (144x144 + tile color) -->
+    <meta name="msapplication-TileImage" content="images/touch/ms-touch-icon-144x144-precomposed.png">
+    <meta name="msapplication-TileColor" content="#3372DF">
+
+    <!-- SEO: If your mobile URL is different from the desktop URL, add a canonical link to the desktop page https://developers.google.com/webmasters/smartphone-sites/feature-phones -->
+    <!--
+    <link rel="canonical" href="http://www.example.com/">
+    -->
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/material.deep_orange-green.min.css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <style>
+    #view-source {
+      position: fixed;
+      display: block;
+      right: 0;
+      bottom: 0;
+      margin-right: 40px;
+      margin-bottom: 40px;
+      z-index: 900;
+    }
+    </style>
+  </head>
+  <body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header mdl-layout__header--scroll mdl-color--primary">
+        <div class="mdl-layout--large-screen-only mdl-layout__header-row">
+        </div>
+        <div class="mdl-layout__header-row">
+          <h3><?php bloginfo( 'name' ); ?><br><p><?php bloginfo( 'description' ); ?><p></h3>
+        </div>
+        <div class="mdl-layout--large-screen-only mdl-layout__header-row">
+        </div>
+        <div class="mdl-layout--large-screen-only mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark">
+          <a href="#overview" class="mdl-layout__tab">Overview</a>
+          <a href="#features" class="mdl-layout__tab">Features</a>
+          <a href="#features" class="mdl-layout__tab">Details</a>
+          <a href="#features" class="mdl-layout__tab">Technology</a>
+          <a href="#features" class="mdl-layout__tab">FAQ</a>
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored mdl-shadow--4dp mdl-color--accent" id="add">
+            <i class="material-icons">add</i>
+          </button>
+        </div>
+      </header>
+      <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title"><?php bloginfo( 'name' ); ?></span>
+        <nav class="mdl-navigation">
+          <a href="#overview" class="mdl-navigation__link">Overview</a>
+          <a href="#features" class="mdl-navigation__link">Features</a>
+          <a href="#features" class="mdl-navigation__link">Details</a>
+          <a href="#features" class="mdl-navigation__link">Technology</a>
+          <a href="#features" class="mdl-navigation__link">FAQ</a>
+        </nav>
+      </div>
--- index.php
+++ index.php
@@ -1,22 +1,30 @@
 <?php get_header(); ?>
-<main class="mdl-layout__content">
-	<div class="mdl-layout__tab-panel is-active" id="overview">
-		<div id="content">
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
-				<div class="mdl-card mdl-cell mdl-cell--12-col">
-	              <div class="mdl-card__supporting-text">
-	                <h4><?php the_title(); ?></h4>
-	                <?php the_content(__('(more...)')); ?>
-	                <br>Posted on <?php the_time('F jS, Y') ?>
-	              </div>
-	              <div class="mdl-card__actions">
-	                <a href="#" class="mdl-button">Continue Reading...</a>
-	              </div>
-	            </div>
-			</section>
-			<?php endwhile; else: ?>
-			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-		</div>
-	</div>
-	<?php get_footer(); ?>
+      <main class="mdl-layout__content">
+        <div class="mdl-layout__tab-panel is-active" id="overview">
+          <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+          <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
+            <header class="section__play-btn mdl-cell mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone mdl-color--teal-100 mdl-color-text--white">
+              <i class="material-icons">play_circle_filled</i>
+            </header>
+            <div class="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone">
+              <div class="mdl-card__supporting-text">
+                <h4><?php the_title(); ?></h4>
+                <?php the_content(__('(more...)')); ?>
+                <br>Posted on <?php the_time('F jS, Y') ?>
+              </div>
+              <div class="mdl-card__actions">
+                <a href="<?php echo esc_url( get_permalink() ); ?>" class="mdl-button">Continue Reading...</a>
+              </div>
+            </div>
+          </section>
+          <?php endwhile; else: ?>
+          <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
+            <div class="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone">
+              <div class="mdl-card__supporting-text">
+                <?php _e('Sorry, no posts matched your criteria.'); ?>   
+              </div>
+            </div>
+          </section>
+          <?php endif; ?>
+        </div>
+<?php get_footer(); ?>
